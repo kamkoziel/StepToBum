c48d7596837b9438044c2037f39326e478442073
Kamil Kozieł <kkoziel@outlook.com>
2019-12-03 17:26:54 +0100

    main files init

---
+++ setup.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from PyQt5.QtWidgets import QApplication
+from src.gui.MainWindow import App
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
